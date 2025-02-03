4ec3b990c6a4d8e594bfdeaa7f6925967b92177e
Mohit Hingorani <Mohithingorani2003@gmail.com>
2025-02-03 10:50:58 +0530

    trying overflow fix

--- src/app/components/SideBar.tsx
+++ src/app/components/SideBar.tsx
@@ -29,7 +29,7 @@ const Sidebar = ({ userId }: { userId: number }) => {
   }, [userDataValue.id]); // Add dependency to ensure it triggers when `userDataValue.id` changes
 
   return (
-    <div className="flex flex-col w-full h-full border shadow-md rounded-[25px] bg-white">
+    <div className="flex flex-col overflow-y-scroll w-full h-full border shadow-md rounded-[25px] bg-white">
       {/* Sidebar Header */}
       <div className="p-4 text-2xl top-10">Friends</div>
 
